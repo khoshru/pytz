5208ad399b8744aa511d42a69c6b751da15b5292
khoshru <dinakhoshru@gmail.com>
2025-09-05 16:11:46 +0330

    Using API to display the time in Tehran and Paris

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+
+def get_tehran_time():
+    tehran_tz = pytz.timezone('Asia/Tehran')
+    return datetime.now(tehran_tz).strftime('%Y-%m-%d %H:%M:%S')
+
+def get_paris_time():
+    paris_tz = pytz.timezone('Europe/Paris')
+    return datetime.now(paris_tz).strftime('%Y-%m-%d %H:%M:%S')
+
+@app.route("/time")
+def show_time():
+    paris_dic = get_paris_time()
+    tehran_dict = get_tehran_time()
+    return f"Paris: {paris_dic} | Tehran: {tehran_dict}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True )
